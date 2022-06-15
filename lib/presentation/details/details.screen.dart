814d84bcd0630fb20c6b47e86c034640ce8fc91f
Marcos <marcos.santos@codenapp.com>
2022-06-15 16:23:20 -0300

    adicionando scroll

--- lib/presentation/details/details.screen.dart
+++ lib/presentation/details/details.screen.dart
@@ -14,37 +14,40 @@ class DetailsScreen extends ViewController<IDetailsController> {
     return LoadingWidget(
       child: Scaffold(
         backgroundColor: const Color.fromARGB(255, 43, 40, 40),
-        body: Column(
-          children: [
-            FotoWidget(),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.only(left: 50, right: 50),
-              child: Column(
-                children: [
-                  StreamBuilder<InfoModel?>(
-                    stream: controller.infos.stream,
-                    builder: (context, snapshot) {
-                      return Text(
-                        snapshot.hasData ? snapshot.data!.name : '',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                        ),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  TypeWidget(),
-                  const SizedBox(height: 30),
-                  InfosWidget(),
-                  const SizedBox(height: 30),
-                  StatsWidget(),
-                ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              FotoWidget(),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 30, right: 30),
+                child: Column(
+                  children: [
+                    StreamBuilder<InfoModel?>(
+                      stream: controller.infos.stream,
+                      builder: (context, snapshot) {
+                        return Text(
+                          snapshot.hasData ? snapshot.data!.name : '',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 30,
+                          ),
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    TypeWidget(),
+                    const SizedBox(height: 30),
+                    InfosWidget(),
+                    const SizedBox(height: 30),
+                    StatsWidget(),
+                    const SizedBox(height: 30)
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
