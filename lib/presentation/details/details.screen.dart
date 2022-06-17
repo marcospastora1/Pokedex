00f3f3b61d1e18716760338e45470a21853ee15b
Marcos <marcos.santos@codenapp.com>
2022-06-17 14:10:57 -0300

    transformando screen em stateful

--- lib/presentation/details/details.screen.dart
+++ lib/presentation/details/details.screen.dart
@@ -1,14 +1,34 @@
 import 'package:ekko/domain/core/abstractions/presentation/controllers/details/details_controller.interface.dart';
+import 'package:ekko/domain/core/models/callback.model.dart';
+import 'package:ekko/domain/core/utils/snackbar.util.dart';
 import 'package:ekko/domain/pokemon/models/infos.model.dart';
+import 'package:ekko/infrastructure/dal/inject.dart';
 import 'package:ekko/presentation/details/widgets/foto.widget.dart';
 import 'package:ekko/presentation/details/widgets/infos.widget.dart';
 import 'package:ekko/presentation/details/widgets/stats.widget.dart';
 import 'package:ekko/presentation/details/widgets/type.widget.dart';
 import 'package:ekko/presentation/shared/loading/loading.widget.dart';
-import 'package:ekko/presentation/shared/view_controller.interface.dart';
 import 'package:flutter/material.dart';
 
-class DetailsScreen extends ViewController<IDetailsController> {
+class DetailsScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _DetailsScreen();
+}
+
+class _DetailsScreen extends State<DetailsScreen> {
+  final controller = Inject.find<IDetailsController>();
+
+  @override
+  void initState() {
+    final onError = CallBack<String>(
+      (err) => SnackbarUtil.showError(err!),
+    );
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      controller.loadPokemonDetails(onError: onError);
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return LoadingWidget(
