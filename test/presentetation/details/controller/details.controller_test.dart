8915fa2fe00a34015ef7201eb96d6d6fffef01fb
Marcos <marcos.santos@codenapp.com>
2022-06-17 14:10:43 -0300

    ajustando os testes

--- test/presentetation/details/controller/details.controller_test.dart
+++ test/presentetation/details/controller/details.controller_test.dart
@@ -1,5 +1,6 @@
 import 'package:ekko/domain/core/abstractions/domain/repositories/pokemon_repository.interface.dart';
 import 'package:ekko/domain/core/abstractions/presentation/controllers/details/details_controller.interface.dart';
+import 'package:ekko/domain/core/exceptions/default.exception.dart';
 import 'package:ekko/domain/pokemon/models/pokemon.model.dart';
 import 'package:ekko/domain/pokemon/models/pokemon_details.mock.dart';
 import 'package:ekko/infrastructure/navigation/bindings/controllers/detais.controller.binding.dart';
@@ -30,38 +31,15 @@ void main() {
     );
   });
 
-  test(
-    'Should load sprites with success',
-    () async {
-      when(
-        () => pokemonRepository.getPokemonDetails(
-          name: detailsController.pokemon?.name ?? detailsController.search!,
-        ),
-      ).thenAnswer((_) async => pokemonDetailMock);
-
-      await detailsController.loadSprites();
-
-      verify(
-        () => pokemonRepository.getPokemonDetails(
-          name: detailsController.pokemon?.name ?? detailsController.search!,
-        ),
-      );
-
-      expect(
-        detailsController.sprites.value,
-        pokemonDetailMock.sprites.other.officialArtWork.frontDefault,
-      );
-    },
-  );
-
-  test('Shoul load types with success', () async {
+  test('Should load pokemon details with success', () async {
+    final onError = CallBackMock<String>();
     when(
       () => pokemonRepository.getPokemonDetails(
         name: detailsController.pokemon?.name ?? detailsController.search!,
       ),
     ).thenAnswer((_) async => pokemonDetailMock);
 
-    await detailsController.loadTypes();
+    await detailsController.loadPokemonDetails(onError: onError);
 
     verify(
       () => pokemonRepository.getPokemonDetails(
@@ -69,42 +47,28 @@ void main() {
       ),
     );
 
-    expect(detailsController.types.value, pokemonDetailMock.types);
-  });
-
-  test('Shoul load stats with success', () async {
-    when(
-      () => pokemonRepository.getPokemonDetails(
-        name: detailsController.pokemon?.name ?? detailsController.search!,
-      ),
-    ).thenAnswer((_) async => pokemonDetailMock);
-
-    await detailsController.loadStats();
-
-    verify(
-      () => pokemonRepository.getPokemonDetails(
-        name: detailsController.pokemon?.name ?? detailsController.search!,
-      ),
-    );
-
-    expect(detailsController.stats.value, pokemonDetailMock.stats);
-  });
-
-  test('Shoul load infos with success', () async {
-    when(
-      () => pokemonRepository.getPokemonDetails(
-        name: detailsController.pokemon?.name ?? detailsController.search!,
-      ),
-    ).thenAnswer((_) async => pokemonDetailMock);
-
-    await detailsController.loadInfos();
-
-    verify(
-      () => pokemonRepository.getPokemonDetails(
-        name: detailsController.pokemon?.name ?? detailsController.search!,
-      ),
+    expect(
+      detailsController.sprites.value,
+      pokemonDetailMock.sprites.other.officialArtWork.frontDefault,
     );
 
     expect(detailsController.infos.value, pokemonDetailMock.infos);
+    expect(detailsController.stats.value, pokemonDetailMock.stats);
+    expect(detailsController.types.value, pokemonDetailMock.types);
+  });
+
+  test('Should load pokemon details with error', () async {
+    final onError = CallBackMock<String>();
+    final err = DefaultException(
+      message: 'Tente escrever o nome do pokemon em inglês',
+    );
+
+    when(() => pokemonRepository.getPokemonDetails(
+            name: detailsController.pokemon?.name ?? detailsController.search!))
+        .thenThrow(err);
+
+    await detailsController.loadPokemonDetails(onError: onError);
+
+    verify(() => onError(err.toString()));
   });
 }
